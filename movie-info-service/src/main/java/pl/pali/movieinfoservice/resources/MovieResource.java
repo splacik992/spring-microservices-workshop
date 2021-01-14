2e822b057e8150af358061c419ac4f1a59e9431c
Rafał Paliwoda <45509510+splacik992@users.noreply.github.com>
2021-01-14 13:16:44 +0100

    Add files via upload

---
+++ movie-info-service/src/main/java/pl/pali/movieinfoservice/resources/MovieResource.java
@@ -0,0 +1,17 @@
+package pl.pali.movieinfoservice.resources;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import pl.pali.movieinfoservice.models.Movie;
+
+@RestController
+@RequestMapping("/movies")
+public class MovieResource {
+
+    @RequestMapping("/{movieId}")
+    public Movie getMovieInfo(@PathVariable("movieId") String movieId){
+        return new Movie(movieId,"Test name");
+
+    }
+}
